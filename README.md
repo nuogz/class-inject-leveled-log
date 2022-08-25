dde4f4a80bda4716aa5ab4305db2e08e0ee82a14
Nuogz <zheung@vip.qq.com>
2022-08-25 16:35:50 +0800

    v1.0.0 * independent from many projects * tweak all files for publishing to npm * start use `CHANGLOG.md` since version `v1.0.0`

---
+++ README.md
@@ -0,0 +1,5 @@
+# @nuogz/class-inject-leveled-log
+![Version](https://img.shields.io/github/package-json/v/nuogz/class-inject-leveled-log?style=flat-square)
+[![License](https://img.shields.io/github/license/nuogz/class-inject-leveled-log?style=flat-square)](https://www.gnu.org/licenses/lgpl-3.0-standalone.html)
+
+Inject base level-based log function into class.
---
+++ index.d.ts
@@ -0,0 +1,16 @@
+export default function LoggerInjecter(target: any, option?: LogOption): void;
+export type SingleLogFunction = (level: string, nameLog: string, ...data: any[]) => any;
+export type LogFunction = (nameLog: string, ...data: any[]) => any;
+export type LogFunctions = {
+	trace: LogFunction;
+	debug: LogFunction;
+	info: LogFunction;
+	warn: LogFunction;
+	error: LogFunction;
+	fatal: LogFunction;
+	mark: LogFunction;
+};
+export type LogOption = {
+	name: string;
+	logger: (false | null) | SingleLogFunction | LogFunctions;
+};
